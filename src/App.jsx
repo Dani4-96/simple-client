81f1ed6c405672e25220942f8384fb4935cd1781
Danil Kaist <kaystd@gmail.com>
2018-12-15 23:38:18 +0700

    added foundation

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
 import React from 'react'
+import { CssBaseline, AppBar, Toolbar, Typography, Button, IconButton } from "@material-ui/core";
+import MenuIcon from '@material-ui/icons/Menu';
 
 export default () => (
-    <h1>Simple react client</h1>
+    <div>
+        <AppBar position="static">
+            <Toolbar>
+                <IconButton color="inherit" aria-label="Menu">
+                    <MenuIcon />
+                </IconButton>
+                <Typography variant="h6" color="inherit">
+                    App
+                </Typography>
+                <Button color="inherit">Button</Button>
+            </Toolbar>
+        </AppBar>
+        <CssBaseline />
+        <h1>Simple react client</h1>
+    </div>
 );
--- src/index.js
+++ src/index.js
@@ -1,5 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { HashRouter as Router } from 'react-router-dom';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { blue } from '@material-ui/core/colors';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './App';
+import rootReducer from './reducers';
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            light: blue[100],
+            main: blue[500],
+            dark: blue[900],
+        },
+    },
+});
+
+const store = createStore(
+    rootReducer,
+    composeWithDevTools(
+        applyMiddleware(thunk)
+));
+
+window.store = store;
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Router>
+            <MuiThemeProvider theme={theme}>
+                <App />
+            </MuiThemeProvider>
+        </Router>
+    </Provider>
+    , document.querySelector('#root')
+);
